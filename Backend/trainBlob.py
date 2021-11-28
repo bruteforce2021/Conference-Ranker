13751c2e0f6ff7e203f27c3dc4b1f6a31176605f
Shravan Sharma <55808378+shravan-sharma-github@users.noreply.github.com>
2021-11-28 22:58:07 +0530

    Updated train_blog.py
    Added debug statements

--- Backend/trainBlob.py
+++ Backend/trainBlob.py
@@ -8,3 +8,13 @@ with open("train.txt", "r") as f1:           # Open our source file
 
     textToLower = text.lower()
 
+# Find all the words and place them into a list
+words = re.findall("[a-z]+", textToLower)
+# Join them into one string
+oneString = " ".join(words)
+
+# The path we want to store our stats file at
+pathToFile = "result.txt"
+# Connect the path to the Spelling object
+spelling = Spelling(path=pathToFile)
+spelling.train(oneString, pathToFile)
