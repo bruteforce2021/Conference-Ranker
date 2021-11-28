54c90a4888c7368b9768aaa1371d30db129a5b65
Yash Agarwal <ayash1998.ya@gmail.com>
2021-11-28 23:54:55 +0530

    Update extractData.py
    Updated/added url fetch for conference data

--- Backend/extractData.py
+++ Backend/extractData.py
@@ -12,7 +12,42 @@ conf_list = []
 with open("data.json") as f:
     conf_list = json.load(f)
 
+url = "http://portal.core.edu.au/conf-ranks/"
 
+for con in data_reader:#100 test
+    # print(con[4])
+    print(url+str(con[0])+"/json")
+
+    if con[0] == "" or con[4]=="":
+        continue
+    response = urlopen(url+str(con[0])+"/json")
+    data_json = json.loads(response.read())
+    # print()
+
+
+    
+
+    conf_list.append({
+        'conf_core_id': con[0],
+        'conf_name': con[1],
+        'conf_reference': con[2],
+        'conf_source': con[3],
+        'conf_url': data_json['url'],
+        'conf_rank': con[4],
+    })
+
+    with open("data.json", 'w') as json_file:
+        json.dump(conf_list, json_file,
+                  indent=4,
+                  separators=(',', ': '))
+
+    # with open('data.json', 'r', encoding='utf-8') as f:
+    #     cur_data = json.loads(f.read())
+    #     cur_data.append(conf_data)
+    # with open('data.json', 'a', encoding='utf-8') as f:
+    #     json.dump(cur_data, f, indent=4)
+
+data.close()
 
 # menu_reader = csv.reader(menu)
 # menu_headings = next(menu_reader)
