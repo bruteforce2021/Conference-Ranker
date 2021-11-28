f2138b9209d88a8ab81d05ac25f163bb822de674
Lalit Gupta <34339674+devLalitx86Repo@users.noreply.github.com>
2021-11-28 22:59:03 +0530

    Update app.py
    Added main query route and train blob

--- Backend/app.py
+++ Backend/app.py
@@ -17,6 +17,67 @@ db = mongo_client.testDB
 test = db['Test']
 
 
+def queryDB(query):
+    res = test.find(
+        {
+            "$text":
+            {
+                "$search": query
+            }
+        },
+        {
+            "score": {"$meta": "textScore"}
+        }).sort(
+        [
+            (
+                'score', {
+                    "$meta": "textScore"
+                }
+            )
+        ]
+    )
+    return res
+
+
+class ResolveQuery(Resource):
+    def post(self):
+
+        # Collecting query data
+        postedData = request.get_json()
+        query = postedData['query']
+
+        # querying database
+        result = queryDB(query)
+
+        # collecting similar words
+        query_blob = TextBlob(query)
+        correct_query = query_blob.correct()
+
+        if result.count() == 0:
+            conf_list = []
+            retmap = {
+                'status': 201,
+                'suggest_word': str(correct_query),
+                'data': conf_list
+            }
+            return retmap
+        else:
+            conf_list = []
+            for conf in result:
+                conf_list.append({"id": conf['conf_core_id'],
+                                  "title": conf['conf_title'],
+                                  "rank": conf['conf_rank'],
+                                  "acronym": conf['conf_acronym'],
+                                  'url': conf['conf_url'],
+                                  'source': conf['conf_source']
+                                  })
+
+            retmap = {
+                'status': 200,
+                'suggest_word': str(correct_query),
+                'data': conf_list
+            }
+            return retmap
 
 
 class Print(Resource):
